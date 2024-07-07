73ef76effc9b6a29600799b316b1ca2b7a409ab1
Alireza Shahryari <eng.alirezashahryari@gmail.com>
2024-07-07 21:22:23 +0330

    update data route for crud operations (create, read, update and delete)

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,56 +1,50 @@
+// app/api/data/route.js
 import { NextResponse } from 'next/server';
 import { v4 as uuidv4 } from 'uuid';
 
+let todos = [
+  {
+    id: uuidv4(),
+    name: 'Learn Next.js',
+    isDone: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Learn HTML',
+    isDone: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Start new sideproject',
+    isDone: false,
+  },
+];
+
 export async function GET(request) {
-    return NextResponse.json({
-        todos: [
-            {
-                id: uuidv4(),
-                name: 'Learn Next.js',
-                isDone: false,
-            },
-            {
-                id: uuidv4(),
-                name: 'Learn HTML',
-                isDone: false,
-            },
-            {
-                id: uuidv4(),
-                name: 'Start new sideproject',
-                isDone: false,
-            },
-        ],
-    });
+  return NextResponse.json({ todos });
 }
 
-export const addTodo = (name) => {
-    let newTodo = {
-        id: uuidv4(),
-        name,
-        isDone: false,
-    };
-    todos.push(newTodo);
+export async function POST(request) {
+  const { name } = await request.json();
+  const newTodo = {
+    id: uuidv4(),
+    name,
+    isDone: false,
+  };
+  todos.push(newTodo);
+  return NextResponse.json({ todo: newTodo });
+}
+
+export const DELETE = async (request) => {
+  const { id } = await request.json();
+  todos = todos.filter((obj) => obj.id !== id);
+  return NextResponse.json({ message: 'Todo deleted' });
 };
 
-export const deleteTodo = (id) => {
-    todos = todos.filter((obj) => {
-        return obj.id !== id;
-    });
-};
-
-export const updateTodo = ({ id, isDone }) => {
-    // only isDone can be updated atm
-    let newTodos = [];
-    todos.map((obj) => {
-        let newTodo = { ...obj };
-        if (obj.id == id) {
-            newTodo = {
-                id,
-                name: obj.name,
-                isDone,
-            };
-        }
-        newTodos.push(newTodo);
-    });
-    todos = newTodos;
+export const PATCH = async (request) => {
+  const { id, isDone } = await request.json();
+  todos = todos.map((obj) =>
+    obj.id === id ? { ...obj, isDone } : obj
+  );
+  return NextResponse.json({ message: 'Todo updated' });
 };
